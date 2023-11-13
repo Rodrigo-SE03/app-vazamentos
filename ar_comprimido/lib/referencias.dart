0c38e7c60ca4a8d7bb7396f8134a75b1e146d0a5
Rodrigo <se03rodrigo@gmail.com>
2023-11-13 16:53:36 -0300

    criação da tela de referências

--- ar_comprimido/lib/referencias.dart
+++ ar_comprimido/lib/referencias.dart
@@ -26,9 +26,58 @@ class Referencias extends StatelessWidget {
                           icon: const Icon(Icons.arrow_circle_left_outlined,
                               color: Colors.white, size: 40))
                     ]))),
-        body: const Center(
+        body: Center(
             child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 90, horizontal: 30),
-                child: Column(children: []))));
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: Column(children: [
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height - 160,
+                      child: ListView(shrinkWrap: true, children: [
+                        Itens('engate_rapido', 'Conexão de Engate Rápido'),
+                        Itens('valvula_solenoide', 'Válvula Solenoide')
+                      ]))
+                ]))));
+  }
+}
+
+// ignore: must_be_immutable
+class Itens extends StatelessWidget {
+  String imagem;
+  String componente;
+  Itens(this.imagem, this.componente, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+            width: 90,
+            height: 120,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/componentes/$imagem.png')))),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: SizedBox(
+                  width: 200,
+                  child: Text(
+                    componente,
+                    textAlign: TextAlign.left,
+                    style: titleFont(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
